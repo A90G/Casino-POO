61f5c88c9f55be0cc73dfb8ac8b40b7a633b944e
Araceli Gittardi <araceligittardi@tdf.edu.ar>
2022-10-24 22:33:17 -0300

    revisar clase jugador

--- jugador.ts
+++ jugador.ts
@@ -1,6 +1,25 @@
 import { Persona } from "./persona";
 
-export class Jugador {
-
-    
+export class Jugador extends Persona {
+    private dinero: number;
+        
+    constructor(pNombre: string, pApellido: string, pEdad: number, pDinero: number) {
+    super (pNombre, pApellido, pEdad);
+    this.dinero = pDinero;
+    }
+public porcentajeOperGanadoras(): number{
+    let porcentaje = (this.dinero * 100) / this.creditoInicial();
+    return porcentaje;
+}
+public subirCategoria(): number{
+    if(this.dinero >= 1000000 && this.porcentajeOperGanadoras() >= 45){
+    return 1;    
+}else{
+    return 0;    
+}
+}
+public creditoInicial(pDinero: number): void{
+    this.dinero = pDinero;
+    console.log(`El jugador ${this.nombre} tiene ${this.dinero} de credito inicial`);
+}
 }
