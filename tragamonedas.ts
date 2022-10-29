c8024a38877e29ee4774d3d221cbe4b3553ad782
Leonel Rivero <103612900+leorivero91@users.noreply.github.com>
2022-10-29 20:09:42 -0300

    tragamonedas listo, poderoso ver bonus

--- tragamonedas.ts
+++ tragamonedas.ts
@@ -1,6 +1,39 @@
-import { Juegos } from "./juegos";
+import { Casino } from './casino';
+import { Persona } from './persona';
 
-export class Tragamonedas {
-
-    
+export class Tragamonedas{
+  protected tematica: string;
+  protected apuestaMinima: number;
+  protected probabilidadGanar: number;
+ 
+  constructor(pTematica: string, pApuestaMinima:number,pProbabilidadGanar:number,){
+    this.tematica=pTematica;
+    this.apuestaMinima=pApuestaMinima;
+    this.probabilidadGanar=pProbabilidadGanar;
+  }
+  public setTematica(pTematica:string):void{
+    this.tematica=pTematica;
+  }
+  public getApuestaMinima():number{
+    return this.apuestaMinima;
+  }
+  public setApuestaMinima(pApuestaMinima:number){
+    this.apuestaMinima=pApuestaMinima;
+  }
+  public jugarTragamonedas(apuesta:number){
+      let lugar1: number = Math.floor(Math.random() * 2) + 1;
+      let lugar2: number = Math.floor(Math.random() * 2) + 1;
+      let lugar3: number = Math.floor(Math.random() * 2) + 1;
+      let combinacion= `${lugar1}${lugar2}${lugar3}`;
+      
+      if (lugar1 === lugar2 && lugar2 ===lugar3) {
+          console.log(`La combinación es ${combinacion}. El usuario ha ganado su apuesta: $${apuesta * 2}`);
+          return apuesta * 2;
+      } else {
+          console.log(`El resultado es ${combinacion}. El usuario ha perdido su apuesta: $${0}`);
+          return 0;
+      } 
+  }
 }
+let Tragamonedas1=new Tragamonedas ('animales',10,1)
+Tragamonedas1.jugarTragamonedas(100);
--- tragamonedas_poderoso.ts
+++ tragamonedas_poderoso.ts
@@ -1,7 +1,16 @@
-import { Juegos } from "./juegos";
 import { Tragamonedas } from "./tragamonedas";
 
-export class Tragamonedas_Poderoso {
+export class Tragamonedas_Poderoso extends Tragamonedas {
+  protected diaDeSuerte: number;
 
-    
+  constructor(pTematica: string, pApuestaMinima:number,pProbabilidadGanar:number,pDiaDeSuerte:number){
+    super(pTematica,pApuestaMinima,pProbabilidadGanar)
+    this.diaDeSuerte= pDiaDeSuerte;
+ }
+  public setDiaDeSuerte(pDiaDeSuerte:number){
+    return this.jugarTragamonedas;
 }
+}
+
+let Tragamonedas1=new Tragamonedas_Poderoso ('animales',2,1,10);
+Tragamonedas1.setDiaDeSuerte(5);
