70345114d51cdfeb86a15587b2d86874500b3636
rcofreces <cofrecesrbles@tdf.edu.ar>
2022-10-31 21:33:35 -0300

    31/10/22

--- casino.js
+++ casino.js
@@ -20,14 +20,17 @@ var Casino = /** @class */ (function () {
     Casino.prototype.setCiudad = function (pCiudad) {
         return this.ciudad = pCiudad;
     };
+    Casino.prototype.getAforo = function () {
+        return this.aforo;
+    };
     Casino.prototype.validacionAforo = function (jugadores) {
         for (var i = 0; i < jugadores.length; i++) {
             if (jugadores[i].esMayorEdad() === true) {
-                jugadores.splice(i, 1);
-                console.log("El jugador ".concat(jugadores[i].getNombre(), " ").concat(jugadores[i].getApellido(), " ha sido retirado del casino ya que tiene ").concat(jugadores[i].getEdad(), " a\u00F1os"));
+                this.aforo.push(jugadores[i]);
+                console.log("El jugador ".concat(jugadores[i].getNombre(), " ").concat(jugadores[i].getApellido(), " ha ingresado en el casino ya que tiene ").concat(jugadores[i].getEdad(), " a\u00F1os"));
             }
         }
-        return jugadores[];
+        return this.aforo;
     };
     return Casino;
 }());
@@ -37,14 +40,15 @@ var jugador002 = new jugador_1.Jugador("Pablo", "Gómes", 28, 250000);
 var jugador003 = new jugador_1.Jugador("Ariel", "Durán", 18, 15000);
 var jugador004 = new jugador_1.Jugador("Romina", "Perez", 16, 2500);
 var jugador005 = new jugador_1.Jugador("Juana", "Dinardo", 30, 55000);
-var jugador006 = new jugador_1.Jugador("María", "Fernandez", 54, 75000);
+var jugador006 = new jugador_1.Jugador("María", "Fernandez", 12, 75000);
 var jugador007 = new jugador_1.Jugador("James", "Bond", 40, 2500000);
-var jugador008 = new jugador_1.Jugador("Luis", "Mendez", 65, 115000);
+var jugador008 = new jugador_1.Jugador("Luis", "Mendez", 46, 115000);
 var jugador009 = new jugador_1.Jugador("Lu", "Chi", 74, 555000);
-var jugador010 = new jugador_1.Jugador("Luciana", "Velez", 60, 50000);
+var jugador010 = new jugador_1.Jugador("Luciana", "Velez", 37, 50000);
 var arreglo_Jugadores = [jugador001, jugador002, jugador003, jugador004, jugador005, jugador006, jugador007, jugador008, jugador009, jugador010];
 var casinoROYALE = new Casino(23, "Buenos Aires");
 casinoROYALE.validacionAforo(arreglo_Jugadores);
+console.log(casinoROYALE.getAforo());
 /* public cuentaCorriente(): void {
    let creditoCuenta = this.dineroFinal - this.creditoInicial();
    console.log(`El jugador ${this.nombre} tiene ${creditoCuenta} de credito en cuenta`);
--- casino.ts
+++ casino.ts
@@ -26,15 +26,19 @@ export class Casino {
    public setCiudad(pCiudad: string): string {
       return this.ciudad = pCiudad;
    }
+
+   public getAforo(): Jugador[] {
+      return this.aforo;
+   }
    
    public validacionAforo (jugadores: Jugador[]) {
       for (let i = 0; i < jugadores.length; i++) {
          if (jugadores[i].esMayorEdad() === true) {
-            jugadores.splice(i, 1);
-            console.log(`El jugador ${jugadores[i].getNombre()} ${jugadores[i].getApellido()} ha sido retirado del casino ya que tiene ${jugadores[i].getEdad()} años`);
+            this.aforo.push(jugadores[i])
+            console.log(`El jugador ${jugadores[i].getNombre()} ${jugadores[i].getApellido()} ha ingresado en el casino ya que tiene ${jugadores[i].getEdad()} años`);
          }
       }
-      return jugadores;
+      return this.aforo;
    }
 }
 
@@ -43,17 +47,19 @@ let jugador002: Jugador = new Jugador("Pablo","Gómes", 28, 250000);
 let jugador003: Jugador = new Jugador("Ariel","Durán", 18, 15000);
 let jugador004: Jugador = new Jugador("Romina","Perez", 16, 2500);
 let jugador005: Jugador = new Jugador("Juana","Dinardo", 30, 55000);
-let jugador006: Jugador = new Jugador("María","Fernandez", 54, 75000);
+let jugador006: Jugador = new Jugador("María","Fernandez", 12, 75000);
 let jugador007: Jugador = new Jugador("James","Bond", 40, 2500000);
-let jugador008: Jugador = new Jugador("Luis","Mendez", 65, 115000);
+let jugador008: Jugador = new Jugador("Luis","Mendez", 46, 115000);
 let jugador009: Jugador = new Jugador("Lu","Chi", 74, 555000);
-let jugador010: Jugador = new Jugador("Luciana","Velez", 60, 50000);
+let jugador010: Jugador = new Jugador("Luciana","Velez", 37, 50000);
 
 let arreglo_Jugadores: Jugador[] =  [jugador001, jugador002, jugador003, jugador004, jugador005, jugador006, jugador007, jugador008, jugador009, jugador010];
 
 let casinoROYALE: Casino = new Casino (23, "Buenos Aires");
 
 casinoROYALE.validacionAforo(arreglo_Jugadores);
+
+console.log(casinoROYALE.getAforo())
 
 /* public cuentaCorriente(): void {
    let creditoCuenta = this.dineroFinal - this.creditoInicial();
